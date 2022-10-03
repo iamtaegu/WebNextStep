716b60ae8ead6f58fa13397beead37fc5c70d6d2
Taegu <gmdals45@gmail.com>
2022-10-03 12:55:50 +0900

    2022/10/03 Chapter7

--- jwp-basic-step2-user-with-mvc-framework/src/main/java/next/controller/CreateUserController.java
+++ jwp-basic-step2-user-with-mvc-framework/src/main/java/next/controller/CreateUserController.java
@@ -15,7 +15,10 @@ public class CreateUserController implements Controller {
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+        User user = new User(
+                req.getParameter("userId"),
+                req.getParameter("password"),
+                req.getParameter("name"),
                 req.getParameter("email"));
         log.debug("User : {}", user);
 
